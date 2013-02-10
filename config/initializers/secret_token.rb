a688df2d0e63bf04d987cc5e19c6e3176eb3a865
Andy Sturges <arsturges@lbl.gov>
2013-02-10 14:40:09 -0800

    initial rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mycarboncap::Application.config.secret_token = '164e5298753dcd613bdd1f28caf6ab3c47bd294b78f0f094f237165876e9dc14cd018916d19c22c62a41a73465d5efe36c3c2862f202c592eddef9c4ece178fc'
