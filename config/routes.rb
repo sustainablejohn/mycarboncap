541ee44c4914f8d5207b83b6013704b831c42560
Andy Sturges <arsturges@lbl.gov>
2013-02-15 20:21:12 -0800

    got rid of /static_pages/ part of url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Mycarboncap::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  #get "static_pages/home"
+  #get "static_pages/help"
+  #get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -9,6 +9,9 @@ Mycarboncap::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+
+  match 'about' => 'static_pages#about'
+  match 'help' => 'static_pages#help'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
@@ -52,7 +55,7 @@ Mycarboncap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
