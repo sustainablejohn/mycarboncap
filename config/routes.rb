6f267db4567bf719ad3484b05495c5aba1be9f26
Andrew Sturges <arsturges@lbl.gov>
2013-03-06 11:36:08 -0800

    changed landing page to feed

--- config/routes.rb
+++ config/routes.rb
@@ -69,7 +69,7 @@ Mycarboncap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'static_pages#landing'
+   root :to => 'static_pages#feed'
 
   # See how all your routes lay out with "rake routes"
 
